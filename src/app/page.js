9f127df4cdfe536fbf4dc01b384f8044a1cbd87e
choiyui <choiyj220220@gmail.com>
2026-08-03 18:48:38 +0900

    chore: 테스트용 페이지 정리

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,6 @@ export default function Home() {
   return (
     <div className="container">
       <h1 className="heading-l-plus">Hello, MO:UM</h1>
-
-      <p className="body-m">Typography Test</p>
     </div>
   );
 }
