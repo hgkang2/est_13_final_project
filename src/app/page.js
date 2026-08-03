e6978f99e3c66591dff7c98f253c7a99d46d756c
choiyui <choiyj220220@gmail.com>
2026-08-04 03:02:47 +0900

    feat: 랜딩 페이지 Footer 컴포넌트 구현

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,3 @@
-import Sidebar from "@/components/layout/Sidebar";
-
-import BottomTab from "@/components/layout/BottomTab";
-
 export default function Home() {
-  return (
-    <main>
-            <Sidebar />
-            <BottomTab />   {" "}
-    </main>
-  );
+  return <main></main>;
 }
