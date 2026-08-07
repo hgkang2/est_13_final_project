5556a5e13679716ecb40305f028f1f23e1de247c
choiyui <choiyj220220@gmail.com>
2026-08-07 16:47:01 +0900

    refactor: JournalCard 컴포넌트 분리

--- src/components/pages/sub-home/SubHome.jsx
+++ src/components/pages/sub-home/SubHome.jsx
@@ -1,11 +1,9 @@
 "use client";
 
-import Image from "next/image";
 import Sidebar from "@/components/layout/Sidebar";
 import BottomTab from "@/components/layout/BottomTab";
 import SubFooter from "@/components/layout/SubFooter";
 import styles from "./SubHome.module.scss";
-import JournalSlider from "@/components/common/JournalSlider";
 import GreetingSection from "./components/GreetingSection";
 import GoalCard from "./components/GoalCard";
 import SpendingSummaryCard from "./components/SpendingSummaryCard";
@@ -14,14 +12,15 @@ import MissionCard from "./components/MissionCard";
 import RecentTransactionsCard from "./components/RecentTransactionsCard";
 import SavingGoalCard from "./components/SavingGoalCard";
 import ChallengeCard from "./components/ChallengeCard";
+import JournalCard from "./components/JournalCard";
 
 export default function SubHome() {
   // UI 개발용 상태값
   const userName = "Moa";
   const hasGoal = false;
   const hasSpendingData = false;
-  const hasSavingGoal = true;
-  const hasChallenge = true;
+  const hasSavingGoal = false;
+  const hasChallenge = false;
   const hasJournal = false;
 
   return (
@@ -49,105 +48,7 @@ export default function SubHome() {
                 <SavingGoalCard hasSavingGoal={hasSavingGoal} />
                 <ChallengeCard hasChallenge={hasChallenge} />
               </div>
-              <article
-                className={styles.journalCard}
-                aria-labelledby="journal-card-title"
-              >
-                <div className={styles.journalInner}>
-                  <header className={styles.journalHeader}>
-                    <div className={styles.journalTitleGroup}>
-                      <h2 id="journal-card-title">
-                        이번 주 소비 절약 그림일기
-                      </h2>
-                    </div>
-
-                    {hasJournal && (
-                      <button type="button" className={styles.moreButton}>
-                        <span>그림일기로 이동</span>
-
-                        <span className="material-icons" aria-hidden="true">
-                          arrow_forward
-                        </span>
-                      </button>
-                    )}
-                  </header>
-
-                  {hasJournal ? (
-                    <JournalSlider />
-                  ) : (
-                    <div className={styles.journalEmpty}>
-                      <div
-                        className={styles.journalPreviewDeck}
-                        aria-hidden="true"
-                      >
-                        <article
-                          className={`${styles.journalPreviewCard} ${styles.journalPreviewCardBack}`}
-                        >
-                          <div className={styles.journalPreviewMeta}>
-                            <time>8/01 (토)</time>
-                            <strong>-17,000원</strong>
-                          </div>
-
-                          <div className={styles.journalPreviewImage}>
-                            <Image
-                              src="/images/journal/journal-06.png"
-                              alt=""
-                              width={140}
-                              height={136}
-                            />
-                          </div>
-
-                          <p className={styles.journalPreviewContent}>
-                            무료 취미 활동으로 즐거운 하루!
-                          </p>
-                        </article>
-
-                        <article
-                          className={`${styles.journalPreviewCard} ${styles.journalPreviewCardFront}`}
-                        >
-                          <div className={styles.journalPreviewMeta}>
-                            <time>8/02 (일)</time>
-                            <strong>--원</strong>
-                          </div>
-
-                          <div
-                            className={`${styles.journalPreviewImage} ${styles.journalPreviewImageEmpty}`}
-                          >
-                            <Image
-                              src="/images/journal/journal-empty.png"
-                              alt=""
-                              width={140}
-                              height={136}
-                            />
-                          </div>
-
-                          <p className={styles.journalPreviewContent}>
-                            오늘도 실천이 기대돼요!
-                          </p>
-                        </article>
-                      </div>
-                      <div className={styles.journalEmptyText}>
-                        <p>이번 주 그림일기를 기다리고 있어요!</p>
-
-                        <span className={styles.journalEmptyDescription}>
-                          오늘의 소비를 기록하면 첫 그림일기가 완성돼요.
-                        </span>
-
-                        <button
-                          type="button"
-                          className={styles.journalEmptyButton}
-                        >
-                          <span>그림일기 보러가기</span>
-
-                          <span className="material-icons" aria-hidden="true">
-                            arrow_forward
-                          </span>
-                        </button>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              </article>
+              <JournalCard hasJournal={hasJournal} />
             </div>
           </div>
         </main>
