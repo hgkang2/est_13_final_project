f591a3c6c09f18bb654e16c51d0b00470c6dfcbc
choiyui <choiyj220220@gmail.com>
2026-08-15 23:17:52 +0900

    refactor: 수정 폼 중복 검증 코드 제거

--- src/components/pages/transaction/components/TransactionDetailEdit/TransactionEdit.jsx
+++ src/components/pages/transaction/components/TransactionDetailEdit/TransactionEdit.jsx
@@ -173,43 +173,6 @@ export default function TransactionEdit({
     onSave(editForm);
   };
 
-  const validateEditForm = form => {
-    const errors = {};
-
-    if (!form.amount) {
-      errors.amount = "금액을 입력해주세요.";
-    } else if (
-      !Number.isFinite(Number(form.amount)) ||
-      Number(form.amount) <= 0
-    ) {
-      errors.amount = "금액은 0보다 큰 숫자로 입력해주세요.";
-    }
-
-    if (!form.category) {
-      errors.category = "카테고리를 선택해주세요.";
-    }
-
-    if (!form.date) {
-      errors.date = "날짜를 선택해주세요.";
-    }
-
-    if (!form.paymentMethod && form.type !== "transfer") {
-      errors.paymentMethod = "결제수단을 선택해주세요.";
-    }
-
-    if (form.type === "transfer") {
-      if (!form.withdrawAccount) {
-        errors.withdrawAccount = "출금 계좌를 선택해주세요.";
-      }
-
-      if (!form.depositAccount) {
-        errors.depositAccount = "입금 계좌를 선택해주세요.";
-      }
-    }
-
-    return errors;
-  };
-
   return (
     <aside className={styles.detailPanel}>
       <div className={styles.detailHeader}>
